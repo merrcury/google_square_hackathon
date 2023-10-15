742a98da8471c44e06f42f4da795b00263a709bd
ankuraxz <34418414+Ankuraxz@users.noreply.github.com>
2023-10-14 20:53:27 -0400

    Corrected Recommend and summary template

--- app/routers/seller.py
+++ app/routers/seller.py
@@ -300,9 +300,33 @@ def get_ingredient_summary():
         template = """ 
         CONTEXT: You are an AI bot provided with a list of ingredients {ingredients}. You need to sum up , group & summarize the list of ingredients.
         TASK: Group up all Ingredients based on their name and type , sum up their quantity and provide a summary of the ingredients.
-        ANSWER: Provide the JSON key-value pairs without special chars ingredient_type1:ingredient_name1:quantity,ingredient_name2:quantity,ingredient_type2:ingredient_name1:quantity,ingredient_name2:quantity. For example: "Vegetables":"Tomato":10,"Potato":20,"Spices":"Salt":10,"Pepper":20
+        ANSWER: Provide the JSON key-value pairs without special chars 
+                (
+            "ingredient_type1": (
+                "name": "ingredient_name1",
+                "quantity": "quantity"
+            ),
+            "ingredient_type2": (
+                "name": "ingredient_name2",
+                "quantity": "quantity"
+            )
+        )
+
+        For example: 
+                (
+            "Vegetables": (
+                (name: "Tomato", quantity: 10),
+                (name: "Potato", quantity: 20)
+            ),
+            "Spices": (
+                (name: "Salt", quantity: 10),
+                (name: "Pepper", quantity: 20)
+            )
+        )
+
         CONSTRAINTS: Keep in mind the summary should be based on ingredient name and type.
         In case of similar names like Tomato and Tomato Puree, group them together, Add their Quantities, like Potato 500 gram, Potato 500 g, Potato 1kg,Potato  8 kilogram, group all and  sum them up like Potato: 10kg.
+        Provide JSON only, no recipie, no code, no special chars.
         """
 
         prompt = PromptTemplate.from_template(template)
